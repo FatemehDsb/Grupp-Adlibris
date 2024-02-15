88944e07c33f26b5cd9713c76cfa5b76385f5e60
Xayida83 <lotta.lindberg83@gmail.com>
2024-02-15 13:35:57 +0100

    Merge branch 'Lotta' of https://github.com/FatemehDsb/Grupp-Adlibris

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ let displayBookCards = () => {
     card.style.fontSize="11px";
     
     card.innerHTML = `
-      <img class="bookCover" src="${book.image}" alt="${book.title}">
-      <h3 class="bookTitle" ><a href="${book.url}">${book.title}</a></h3>
-      <p class="bookAuthor" >${book.author}</p>
-      <button class="buyLinkButton">${book.price} kr <i class="fas fa-shopping-bag"></button>`
+      <img class="book-cover" src="${book.image}" alt="${book.title}">
+      <h3 class="book-title" ><a href="${book.url}">${book.title}</a></h3>
+      <p class="book-author" >${book.author}</p>
+      <button class="buy-link-button">${book.price} kr <i class="fas fa-shopping-bag"></button>`
       ;
 
       document.body.append(card);
